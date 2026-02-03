c33e14e491b6a1bfb0e30b8522bab87a22eda1c7
dingky duo amurao <amurao.dingky.duo@asuene.com>
2026-02-03 15:04:32 +0800

    initial setUp

---
+++ medusa-config.ts
@@ -0,0 +1,35 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+// Use 'export default' instead of 'module.exports'
+export default defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!, // Ensure your .env has no trailing slash
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+    // Required for cross-port login (7000 to 9000)
+    cookieOptions: {
+      secure: false, 
+      sameSite: "lax",
+    }
+  },
+ // Plugins MUST be in this array in v2
+  plugins: [
+    {
+      resolve: "@medusajs/dashboard",
+      options: {
+        // Leave empty for default, or specify custom path
+      },
+    },
+  ],
+  modules: [ 
+    { resolve: "./modules/restaurant" },
+    { resolve: "./modules/delivery" } 
+  ]
+})
